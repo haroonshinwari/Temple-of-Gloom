1d9cffb3c491cbd513f030cbc70ebe8be93d4957
Haroon Shinwari <Haroon@Haroons-MacBook-Pro.local>
2018-03-28 19:46:24 +0100

    added a print statement for step back using stack

--- src/student/Explorer.java
+++ src/student/Explorer.java
@@ -57,24 +57,20 @@ public class Explorer {
 
             List<NodeStatus> nodes = (ArrayList) state.getNeighbours();       // a list holding all neighbours of the current tile
             for(NodeStatus n:nodes) {                                         // loop through all the neighbours
-                if(!visitedTiles.contains(n.getId())){                        // if this neighbour has not been visited before
+                if (!visitedTiles.contains(n.getId())) {                        // if this neighbour has not been visited before
                     long nextMove = n.getId();
                     state.moveTo(nextMove);                                   // move to the next available neighbour
                     routeStack.add(nextMove);                                 //add the next move to the stack
                     System.out.println("The next move is: " + nextMove);
                     break;
+                }
             }
 
-            if (x = false);
+            if (x = false) {
                 routeStack.pop();                                           // pop off the last current tile from stack
                 state.moveTo(routeStack.peek());                            //move one step back
-
-
-
-
-
-
-
+                System.out.println("Moving from stack one step back to: " + routeStack.peek());
+            }
 
         }
 
